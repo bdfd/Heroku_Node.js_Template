17904b14486c5c9ebefc3b6093bd5a9475f983da
bdfd <bdfd2005@gmail.com>
2022-02-03 13:48:59 -0500

    ✨ feat(HTML page): Enable Render HTML
    enable render a html as independently

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 /*
  * @Author: BDFD
  * @Date: 2022-02-03 12:27:46
- * @LastEditTime: 2022-02-03 13:22:44
+ * @LastEditTime: 2022-02-03 13:47:56
  * @LastEditors: BDFD
  * @Description:
  * @FilePath: \Heroku_Node.js_Template\routes\index.js
  */
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 //router address localhost:3000/
 //descriptions: Home Page
@@ -31,6 +33,16 @@ router.get('/contact', function (req, res, next) {
 /* GET test page. */
 router.get('/test', function (req, res, next) {
 	res.render('test', { layout: 'layouts/hero_layout' });
+});
+
+//router address localhost:3000/test2
+//descriptions: Test Page for render HTML page
+//comments: try out for render HTML independently
+/* GET test02 page. */
+router.get('/test02', function (req, res, next) {
+	res.sendFile('testscript.html', {
+		root: path.join(__dirname, '../views/html_files'),
+	});
 });
 
 module.exports = router;
